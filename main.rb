b6b54cf4b9992d545f85661fa75489ddced3a44c
Umar Ghouse <m.umarghouse@gmail.com>
2020-06-06 16:16:13 +0530

    Added code to main to run new modules

--- main.rb
+++ main.rb
@@ -6,7 +6,9 @@ Dir["./lib/*.rb"].each { |file| require file }
 
 list_of_functions = [
   "Combine into a single mailing list",
-  "Combine into a single column"
+  "Combine into a single column",
+  "Check similarities between 2 mailing lists",
+  "Check differences between 2 mailing lists"
 ]
 
 split_data = Split.split
@@ -39,8 +41,13 @@ when "Combine into a single mailing list"
   result = ['combined_mailing_list', Combine_to_mailing_list.combine(split_data)]
 when "Combine into a single column"
   result = ['combined_to_column', Combine_to_single_column.combine(split_data)]
+when "Check similarities between 2 mailing lists"
+  result = ['emails_in_both_lists', Check_similarities.check_similarities(split_data)]
+when "Check differences between 2 mailing lists"
+  result = ['difference_between_lists', Check_differences.check_difference(split_data)]
 else
   puts "Invalid function"
 end
 
+# puts result[1]
 Write_to_csv.write_file(result[0], result[1])
