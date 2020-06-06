bf3239b918335d3268f14ec9e3452ab4de2c41d9
Umar Ghouse <m.umarghouse@gmail.com>
2020-06-06 13:44:06 +0530

    Added write to csv module
    - Module to write the output to a CSV and save it in the exports folder

--- lib/write_to_csv.rb
+++ lib/write_to_csv.rb
@@ -1,3 +1,9 @@
 # Module to write the output of other modules to a CSV
 
-
+class Write_to_csv
+  def self.write_file(filename, export_data)
+    File.open("./exports/#{filename}.csv", 'w+') do |file|
+      file << export_data.to_s
+    end
+  end
+end
